15b043031f8c397db591843bcea78b0482ab5a57
Rostislav Scheglov <rshcheglov@intersog.com>
2021-06-02 13:21:28 +0300

    Test coomit

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+const  TestMong = (props)=>{
+    
+    return(
+        <div>
+            <input  type = 'text' placeholder="Post Title" />
+            <button type='submit' id="createPost_button_submit" >Submit</button>
+        </div>
+    )
+
+}
+
+export default TestMong;
